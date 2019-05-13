aa2d18e6efcce043c8edd76661dc1b787bb4f2dc
Geoff Smith <smith260194@gmail.com>
2019-05-13 22:05:59 +0100

    Adding a roll method

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Dice do
     my_dice_game = Dice.new
     expect(my_dice_game.class).to eq(Dice)
   end
+
+  it "can respond to roll call" do
+    my_dice = Dice.new
+    expect(my_dice).to respond_to(:roll)
+  end
+
+  # RSpec one line syntax
+  it {is_expected.to respond_to(:roll)}
+
+  it "roll should return a number between 1 and 6" do
+    my_dice = Dice.new
+    roll_result = my_dice.roll
+    expect(roll_result).to be_between(1,6)
+  end
 end
