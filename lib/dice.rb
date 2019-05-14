7ba592721300ee1196baa20067b95923f83e8b4b
Geoff Smith <smith260194@gmail.com>
2019-05-14 08:28:48 +0100

    Allowing number_of_die to be a paramter in the constructor | returning an array instead of a single integer

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,7 +1,12 @@
 class Dice
   def initialize(number_of_die)
+    if (number_of_die.is_a? Integer) && number_of_die >= 0
+      @die = number_of_die
+    else
+      @die = 1
+    end
   end
   def roll
-    1
+    [1]
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -14,10 +14,40 @@ RSpec.describe Dice do
   it "roll should return a number between 1 and 6" do
     my_dice = Dice.new(1)
     roll_result = my_dice.roll
-    expect(roll_result).to be_between(1,6)
+    expect(roll_result[0]).to be_between(1,6)
   end
 
   it "should be able to accept an argument in the constructor" do
     my_dice = Dice.new(1)
+  end
+
+  it "should return 1 from instance variable called die" do
+    my_dice = Dice.new(1)
+    number_of_die = my_dice.instance_variable_get(:@die)
+    expect(number_of_die).to eq(1)
+  end
+
+  it "should return 2 from instance variable called die" do
+    my_dice = Dice.new(2)
+    number_of_die = my_dice.instance_variable_get(:@die)
+    expect(number_of_die).to eq(2)
+  end
+
+  it "should default to 1 die if a non number is passed" do
+    my_dice = Dice.new("hdqdsakjsdh")
+    number_of_die = my_dice.instance_variable_get(:@die)
+    expect(number_of_die).to eq(1)
+  end
+
+  it "should default to 1 die if a negative number is passed" do
+    my_dice = Dice.new(-1)
+    number_of_die = my_dice.instance_variable_get(:@die)
+    expect(number_of_die).to eq(1)
+  end
+
+  it "should return an array of die rolls" do
+    my_dice = Dice.new(1)
+    roll_result = my_dice.roll
+    expect(roll_result.class).to eq(Array)
   end
 end
